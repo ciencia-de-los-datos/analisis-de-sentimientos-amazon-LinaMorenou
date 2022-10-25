b30d7c29e612ffa85cfdbca7f200593e3bafe9f9
LinaMorenou <111617441+LinaMorenou@users.noreply.github.com>
2022-10-25 18:37:31 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -17,26 +17,19 @@ def pregunta_01():
     """
 
     # Lea el archivo `amazon_cells_labelled.tsv` y cree un DataFrame usando pandas.
-    # Etiquete la primera columna como `msg` y la segunda como `lbl`. Esta función
+    # Etiquete la primera columna como `msg` y la segunda como `lbl`. Esta funciÃ³n
     # retorna el dataframe con las dos columnas.
     df = pd.read_csv(
+        # 'C:/Users/jlopezl/OneDrive - Renting Colombia S.A/Archivos/Personal/Especialización/Ciencia de los datos/analisis-de-sentimientos-amazon-JuanesLopez/amazon_cells_labelled.tsv',
         'amazon_cells_labelled.tsv',
         sep='\t',
-        header= None,
-        names=['msg', 'lbl'],
+        header=None,
+        names=['msg','lbl'],
     )
 
     # Separe los grupos de mensajes etiquetados y no etiquetados.
-    df = pd.read_csv(
-        'amazon_cells_labelled.tsv',
-        sep='\t',
-        header= None,
-        names=['msg', 'lbl'],
-    )
-
-    # Separe los grupos de mensajes etiquetados y no etiquetados.
-    df_tagged = df[df["lbl"].notna()]
-    df_untagged = df[df["lbl"].isna()]
+    df_tagged = df[df["lbl"].notnull()]
+    df_untagged = df[df["lbl"].isnull()]
 
     x_tagged = df_tagged["msg"]
     y_tagged = df_tagged["lbl"]
@@ -46,6 +39,7 @@ def pregunta_01():
 
     # Retorne los grupos de mensajes
     return x_tagged, y_tagged, x_untagged, y_untagged
+
 
 def pregunta_02():
     """
@@ -57,15 +51,15 @@ def pregunta_02():
     from sklearn.model_selection import train_test_split
 
     # Cargue los datos generados en la pregunta 01.
-    x_tagged, y_tagged, _, _ = pregunta_01()
+    x_tagged, y_tagged, x_untagged, y_untagged = pregunta_01()
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 12345. Use el 10% de patrones para la muestra de prueba.
     x_train, x_test, y_train, y_test = train_test_split(
-    x_tagged,
-    y_tagged,
-    test_size=0.10,
-    random_state=12345,
+        x_tagged,
+        y_tagged,
+        test_size=0.1,
+        random_state=12345,
     )
 
     # Retorne `X_train`, `X_test`, `y_train` y `y_test`
@@ -78,17 +72,15 @@ def pregunta_03():
     -------------------------------------------------------------------------------------
     """
     # Importe el stemmer de Porter
-    # Importe CountVectorizer
     from nltk.stem.porter import PorterStemmer
+    # Importe CountVectorizer
     from sklearn.feature_extraction.text import CountVectorizer
 
     # Cree un stemeer que use el algoritmo de Porter.
     stemmer = PorterStemmer()
 
-    vectorizer = CountVectorizer()
-
     # Cree una instancia del analizador de palabras (build_analyzer)
-    analyzer = vectorizer.build_analyzer()
+    analyzer = CountVectorizer().build_analyzer()
 
     # Retorne el analizador de palabras
     return lambda x: (stemmer.stem(w) for w in analyzer(x))
@@ -96,7 +88,7 @@ def pregunta_03():
 
 def pregunta_04():
     """
-    Especificación del pipeline y entrenamiento
+    Especificacion del pipeline y entrenamiento
     -------------------------------------------------------------------------------------
     """
 
@@ -108,6 +100,7 @@ def pregunta_04():
     from sklearn.model_selection import GridSearchCV
     from sklearn.pipeline import Pipeline
     from sklearn.naive_bayes import BernoulliNB
+
     # Cargue las variables.
     x_train, _, y_train, _ = pregunta_02()
 
@@ -116,14 +109,14 @@ def pregunta_04():
 
     # Cree una instancia de CountVectorizer que use el analizador de palabras
     # de la pregunta 3. Esta instancia debe retornar una matriz binaria. El
-    # límite superior para la frecuencia de palabras es del 100% y un límite
+    # limite superior para la frecuencia de palabras es del 100% y un limite
     # inferior de 5 palabras. Solo deben analizarse palabras conformadas por
     # letras.
-     countVectorizer = CountVectorizer(
+    countVectorizer = CountVectorizer(
         analyzer=analyzer,
         lowercase=True,
         stop_words="english",
-        token_pattern="(?u)\b\w\w+\b",
+        token_pattern=r"\b\w\w+\b",
         binary=True,
         max_df=1.0,
         min_df=5,
@@ -132,21 +125,21 @@ def pregunta_04():
     # Cree un pipeline que contenga el CountVectorizer y el modelo de BernoulliNB.
     pipeline = Pipeline(
         steps=[
-            ("countvect", countVectorizer),
-            ("bern", BernoulliNB()),
+            ("CountVectorizer", countVectorizer),
+            ("BernoulliNB", BernoulliNB()),
         ],
     )
 
-    # Defina un diccionario de parámetros para el GridSearchCV. Se deben
-    # considerar 10 valores entre 0.1 y 1.0 para el parámetro alpha de
+    # Defina un diccionario de parametros para el GridSearchCV. Se deben
+    # considerar 10 valores entre 0.1 y 1.0 para el parametro alpha de
     # BernoulliNB.
-     param_grid = {
-        "bern__alpha": np.arange(0.1, 1.1, 0.1),
+    param_grid = {
+        "BernoulliNB__alpha": np.linspace(0.1,1,10)
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
-    # parámetros. Use cv = 5, y "accuracy" como métrica de evaluación
-    gridSearchCV = GridSearchCV(
+    # parÃ¡metros. Use cv = 5, y "accuracy" como mÃ©trica de evaluaciÃ³n
+    gridSearchCV =  GridSearchCV(
         estimator=pipeline,
         param_grid=param_grid,
         cv=5,
@@ -155,7 +148,7 @@ def pregunta_04():
         return_train_score=False,
     )
 
-    # Búsque la mejor combinación de regresores
+    # Busque la mejor combinacion de regresores
     gridSearchCV.fit(x_train, y_train)
 
     # Retorne el mejor modelo
@@ -169,7 +162,7 @@ def pregunta_05():
     """
 
     # Importe confusion_matrix
-    from ____ import ____
+    from sklearn.metrics import confusion_matrix
 
     # Obtenga el pipeline de la pregunta 3.
     gridSearchCV = pregunta_04()
@@ -178,14 +171,14 @@ def pregunta_05():
     X_train, X_test, y_train, y_test = pregunta_02()
 
     # Evalúe el pipeline con los datos de entrenamiento usando la matriz de confusion.
-    cfm_train = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_train = confusion_matrix(
+        y_true=y_train,
+        y_pred=gridSearchCV.predict(X_train),
     )
 
-    cfm_test = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_test = confusion_matrix(
+        y_true=y_test,
+        y_pred=gridSearchCV.predict(X_test),
     )
 
     # Retorne la matriz de confusion de entrenamiento y prueba
@@ -194,7 +187,7 @@ def pregunta_05():
 
 def pregunta_06():
     """
-    Pronóstico
+    Pronostico
     -------------------------------------------------------------------------------------
     """
 
@@ -206,7 +199,8 @@ def pregunta_06():
 
     # pronostique la polaridad del sentimiento para los datos
     # no etiquetados
-    y_untagged_pred = ____.____(____)
+    y_untagged_pred = gridSearchCV.predict(X_untagged)
+
 
     # Retorne el vector de predicciones
     return y_untagged_pred
